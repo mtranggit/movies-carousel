fa907b2bd99c1e437628e7265cf42b2f3348ef52
Michael Trang <michael.trang@hotmail.com>
2023-03-30 17:56:30 +1100

    setup react movies carousel

---
+++ apps/carousel/src/app/app.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div className={styles.container}>This soon to be a cool movies carousel!</div>
+  );
+}
+
+export default App;
